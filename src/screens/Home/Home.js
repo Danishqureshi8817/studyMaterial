5c1133b937d3d5dd3f4e09c4dfa54a68887bbc73
zuber qureshi <zuberqureshi95@gmail.com>
2023-08-15 23:06:03 +0530

    api changes

--- src/screens/Home/Home.js
+++ src/screens/Home/Home.js
@@ -1,5 +1,6 @@
-import { StyleSheet, Text, View,Button,ScrollView } from 'react-native'
-import React,{useContext} from 'react'
+import { View, Alert, Text, Button, Pressable, SafeAreaView, ScrollView, Image, TouchableOpacity, Modal, TouchableHighlight, ToastAndroid, BackHandler, Linking } from 'react-native'
+ import { useLayoutEffect, useState, useEffect, useRef } from "react";
+
 import Services from '../../utiles/Services'
 import { AuthContext } from '../../utiles/AuthContext'
 import WelcomeHeader from '../../components/WelcomeHeader'
@@ -9,9 +10,59 @@ import Colors from '../../Shared/Colors'
 import Slider from '../../components/Slider'
 import BranchList from '../../components/BranchList'
 
+import CallApi, {setToken ,CallApiJson } from '../../utiles/network';
+import EncryptedStorage from 'react-native-encrypted-storage';
+ import Loader from '../../components/common/loader/Loader';
+ import DeviceCountry, { TYPE_TELEPHONY} from 'react-native-device-country';
+ import VersionCheck from 'react-native-version-check';
+
 const Home = () => {
 
-  const {userData,setUserData} = useContext(AuthContext)
+   const [banner, setBanner] = useState()
+   const [branch, setBranches] = useState()
+  const banners = async () => {
+    const banners = await CallApiJson('banner', 'GET');
+   console.log("Banner data",banners);
+     setBanner(banners?.data);
+   }
+   const branches = async () => {
+    const branchData = await CallApiJson('semesterList', 'GET');
+   console.log("semesterList data",branchData);
+   setBranches(branchData?.data);
+   }
+
+   const checkUpdateNeeded = async () => {
+    const latestVersion = await VersionCheck.getLatestVersion();
+    const currentVersion = VersionCheck.getCurrentVersion()
+    let updateNeeded = await VersionCheck.needUpdate();
+    console.log('checkUpdateNeeded-latestVersion', latestVersion, 'userSettings?.data?.version_control_froce', userSettings?.data, 'currentVersion', currentVersion, 'updateNeeded', updateNeeded)
+    if ((updateNeeded.isNeeded)) {
+      Alert.alert('Update Required ',
+        'Download Latest Version From PlayStore',
+        [
+          {
+            text: 'Update Now ',
+            onPress: () => {
+              BackHandler.exitApp();
+              Linking.openURL(updateNeeded.storeUrl);
+            }
+          }
+        ],
+        { cancelable: false }
+
+
+      );
+    }
+  }
+  useEffect(() => {
+    banners();
+    branches();
+
+   // checkUpdateNeeded()
+    return () => {
+      console.log('return')
+    }
+  }, [])
 
   // #F6F8FC
   return (
@@ -30,4 +81,4 @@ const Home = () => {
 
 export default Home
 
-const styles = StyleSheet.create({})
+ 
---
+++ src/utiles/network.js
@@ -0,0 +1,93 @@
+import EncryptedStorage from 'react-native-encrypted-storage';
+
+const API_uri = 'https://studymaterial.newindiagyan.online/api/';
+function CallApi(endpoint, method = 'GET', body = null, token = null) {
+  const toUrlEncoded = obj =>
+    Object.keys(obj)
+      .map(k => encodeURIComponent(k) + '=' + encodeURIComponent(obj[k]))
+      .join('&');
+  body = body ? toUrlEncoded(body) : null;
+
+  return new Promise(resolve => {
+    let headres = {
+      'Content-Type': 'application/x-www-form-urlencoded',
+      'Access-Control-Request-Headers': 'Authorization',
+      Authorization: 'Bearer ' + (token ? token : ''),
+    };
+    fetch(API_uri + endpoint, {
+      method: method,
+      headers: headres,
+      body: body,
+    })
+      .then(r => r.json())
+      .then(response => {
+        resolve(response);
+      })
+      .catch(e => {
+        // console.log(e);
+        resolve({
+          status: 500,
+          message: e.message,
+        });
+      });
+  });
+}
+
+function CallApiJson(endpoint, method = 'GET', body = null, token = null) {
+  const toUrlEncoded = obj =>
+    Object.keys(obj)
+      .map(k => encodeURIComponent(k) + '=' + encodeURIComponent(obj[k]))
+      .join('&');
+
+  body = body ? JSON.stringify(body) : null;
+   console.log( 'networkbody', body)
+   return new Promise(resolve => {
+    let headres = {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json',
+      'Access-Control-Request-Headers': 'Authorization',
+      Authorization: 'Bearer ' + (token ? token : ''),
+    };
+    fetch(API_uri + endpoint, {
+      method: method,
+      headers: headres,
+      body: body,
+    })
+      .then(r => r.json())
+      .then(response => {
+        resolve(response);
+      })
+      .catch(e => {
+        // console.log(e);
+        resolve({
+          status: 500,
+          message: e.message,
+        });
+      });
+  });
+}
+
+
+async function setToken(token) {
+   return EncryptedStorage.setItem('token', JSON.stringify(token) )
+    .then(() => 'success')
+    .catch(e => 'error');
+}
+
+
+async function getToken() {
+  const token = await EncryptedStorage.getItem('token');
+   return token;
+}
+
+
+async function removeToken() {
+  const token = await EncryptedStorage.removeItem('token');
+      console.log("Token Removed",token)
+  return token;
+}
+
+
+export default CallApi  ;
+ 
+export {setToken, getToken , removeToken ,CallApiJson};
