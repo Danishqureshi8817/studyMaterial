1def8013cc51034b250d3400d72555637226e3d7
Danish Qureshi <85394659+Danishqureshi8817@users.noreply.github.com>
2023-09-08 10:40:57 +0530

    j

--- src/screens/Home/Home.js
+++ src/screens/Home/Home.js
@@ -3,7 +3,7 @@ import React,{useContext} from 'react'
 import Services from '../../utiles/Services'
 import { AuthContext } from '../../utiles/AuthContext'
 import WelcomeHeader from '../../components/WelcomeHeader'
-import { responsiveWidth } from 'react-native-responsive-dimensions'
+import { responsiveHeight, responsiveWidth } from 'react-native-responsive-dimensions'
 import SearchBar from '../../components/SearchBar'
 import Colors from '../../Shared/Colors'
 import Slider from '../../components/Slider'
@@ -15,16 +15,21 @@ const Home = () => {
 
   // #F6F8FC
   return (
-    <ScrollView style={{padding:responsiveWidth(5),}} >
-      <WelcomeHeader/>
-      <SearchBar/>
+
+    <View style={{flex:1,paddingHorizontal:responsiveWidth(3),paddingTop:responsiveHeight(2)}} >
+
+<WelcomeHeader/>
+      <SearchBar />
+      <ScrollView showsVerticalScrollIndicator={false}>
+
       <Slider />
       <BranchList />
        <BranchList />
         <BranchList />
-   
-   
+
     </ScrollView>
+    </View>
+
   )
 }
 
